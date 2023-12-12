e8b2b49e3e9482899efdd6e0f2913b60ae3908c7
osterhoutan-UofU <andrew.osterhout@utah.edu>
2023-12-12 13:34:08 -0800

    ran with trace file changes

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/bash
 
+#flux: -N1
+#flux: -n1
+#flux: -g1
+#flux: -c8
+
+source $HOME/.envzsh
 cd $HOME/repos/scabbard
+
+export SCABBARD_PATH="$(pwd)/build/libtrace"
+
+$ROCM_PATH/bin/hipcc -fpass-plugin=build/instr/libinstr.so -Lbuild/libtrace -ltrace -pthread -x hip -std=c++17 -g -o test/test.instr.out test/test.cpp
+
 
 export SCABBARD_INSTRUMENTED_EXE_NAME="$(pwd)/test/test.instr.out"
 export SCABBARD_TRACE_FILE="$(pwd)/test/test.scabbard.trace"
