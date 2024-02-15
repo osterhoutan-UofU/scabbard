a7d6d7b8550f990d77c3394bfb851336ddd17ff9
osterhoutan-UofU <andrew.osterhout@utah.edu>
2024-02-15 13:07:18 -0800

    merging with tioga

---
+++ libtrace/include/scabbard/trace/DeviceTracker.hpp
@@ -0,0 +1,25 @@
+/**
+ * @file DeviceTracker.hpp
+ * @author osterhoutan (osterhoutan+scabbard@gmail.com)
+ * @brief an object to be passed through the device calls to keep track of the data
+ * @version alpha 0.0.1
+ * @date 2024-02-15
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+
+namespace scabbard {
+namespace trace {
+namespace device {
+
+  struct DeviceTracker {
+    
+  };
+
+
+
+} //?namespace device
+} //?namespace trace
+} //?namespace scabbard
