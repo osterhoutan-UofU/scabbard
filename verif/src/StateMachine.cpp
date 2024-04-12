6f35da804dca8adf633e0b0c3e934e322b89c0e4
osterhoutan-UofU <andrew.osterhout@utah.edu>
2024-04-12 14:27:08 -0700

    new test minor changes

--- verif/src/StateMachine.cpp
+++ verif/src/StateMachine.cpp
@@ -111,14 +111,15 @@ namespace verif {
 
   const StateMachine::ResultStatus StateMachine::check_race_read(const TraceData& r, const TraceData& o) 
   {
+    mem[o.ptr] = &r;
     if (o.data & InstrData::WRITE) { // if mem stores a write event 
-      if (last_global_sync > o.time_stamp) // that occurred after the last global sync event
+      if (last_global_sync >= o.time_stamp) // that occurred after the last global sync event
           return ResultStatus::WARNING; // return a warning
       auto res = last_stream_sync.find(o.threadId.device.job.STREAM);
       if (res != last_stream_sync.end() && res->second > o.time_stamp) // or that happened after the last stream sync event 
         return ResultStatus::WARNING; // return a warning
     } // else    // if a read event we don't care yet (could be a double read)
-      mem[o.ptr] = &r;
+      // mem[o.ptr] = &r;
       return ResultStatus::GOOD;
   }
 
