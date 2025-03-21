14b943dcc462b6e59ccee4a7d0e0e8f689d38128
osterhoutan-UofU <andrew.osterhout@utah.edu>
2025-03-21 13:08:19 -0700

    got ignacio's test working on my main machine - had to specify to use hipcc as the C++ compiler

---
+++ test/ignacio/test/HIPExample.cpp
@@ -0,0 +1,68 @@
+/**
+ * @file memcpy-no-race.cpp
+ * @author osterhoutan (osterhoutan+scabbard@gmail.com)
+ * @brief a simple test using device memory only and moving mem with hipMemcpy
+ * @version alpha 0.0.1
+ * @date 2024-04-16
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <hip/hip_runtime.h>
+
+#include <thread>
+#include <chrono>
+#include <string>
+#include <iostream>
+
+#define DIM (32ul)
+
+__global__
+auto matrix_mul(double* A, double* B, double* C) -> void
+{
+  const size_t ROW = blockIdx.y*blockDim.y+threadIdx.y;
+  const size_t COL = blockIdx.x*blockDim.x+threadIdx.x;
+  double tmp_sum = 0.0L;
+  for (size_t i=0; i<DIM; ++i)
+    tmp_sum += A[ROW*DIM + i] * B[i*DIM + COL];
+  C[ROW*DIM + COL] = tmp_sum;    // <<<<<<<<<<<<<<<<<<<<<   GPU WRITE LOCATION
+}
+
+__host__
+auto HIP_CHECK(hipError_t hipRes, const std::string& errMsg) -> void
+{
+  if (hipRes == hipSuccess) return;
+  std::cerr << "\n[hip ERR: " << hipRes << "] " << errMsg << std::endl;
+  exit(EXIT_FAILURE);
+}
+
+__host__
+auto main() -> int 
+{
+  double* A, * B, * C;
+  double out[DIM*DIM];
+
+  HIP_CHECK(hipMalloc(&A, sizeof(double)*DIM*DIM), "from `hipMalloc(&A, ...)`");
+  HIP_CHECK(hipMalloc(&B, sizeof(double)*DIM*DIM), "from `hipMalloc(&B, ...)`");
+  HIP_CHECK(hipMalloc(&C, sizeof(double)*DIM*DIM), "from `hipMalloc(&C, ...)`");
+
+  matrix_mul<<<(dim3){1u,1u,1u},(dim3){DIM,DIM,1u},0ul,0ul>>>(A,B,C);
+
+  // HIP_CHECK(hipDeviceSyncronize();, "from `hipDeviceSyncronize()`");  // <<<<<<<<<<<< (MISSING) CPU/GPU SYNC CALL
+  
+  double res_sum = 0.0L;
+  for (int64_t i=(DIM*DIM)-1l; i>=0l; --i) // iterating backwards should ensure that we read something before a write.
+    res_sum += C[i];  // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<  CPU READ LOCATION
+
+  HIP_CHECK(hipStreamSynchronize(0ul), "from `hipStreamSynchronize(0ul)`");
+
+  HIP_CHECK(hipFree(A), "from `hipFree(A)`");
+  HIP_CHECK(hipFree(B), "from `hipFree(B)`");
+  HIP_CHECK(hipFree(C), "from `hipFree(C)`");
+
+  std::cout << res_sum <<std::endl;
+  
+  return 0;
+}
+
---
+++ test/ignacio/test/run.sh
@@ -0,0 +1,1 @@
+export SCABBARD_PATH=/usr/workspace/wsrzc/laguna/scabbard/scabbard/build/scabbard
